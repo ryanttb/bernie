7301b08fa13b3a1f027019bbdfac9ebd943e079a
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2015-06-27 01:57:09 -0400

    parsing data

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 $( function () {
   $( '#prepare' ).click( function( ) {
-    alert( 'prepare!') ;
+    var urls_src = $( '#urls' ).val();
+
+    var urls = urls_src.split( '\n' );
+
+    var preparedHtml = '';
+    var urlParts = '';
+
+    $( urls ).each( function( ) {
+      urlParts = this.split( '\t' );
+
+      preparedHtml += '<tr>';
+      preparedHtml += '<td class="prepared-uid">' + urlParts[ 0 ] + '</td>';
+      preparedHtml += '<td class="prepared-url">' + urlParts[ 1 ] + '</td>';
+      preparedHtml += '<td class="prepared-urn">https://nrs.harvard.edu/' + urlParts[ 0 ] + '</td>';
+      preparedHtml += '</tr>';
+    } );
+
+    $( '#prepared tbody' ).append( preparedHtml );
   } );
 } );
