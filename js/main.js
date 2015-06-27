89f5bd87095813c886dcfc1f7136d6e8857d5ca5
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2015-06-27 14:42:55 -0400

    submit urls via iframes

--- js/main.js
+++ js/main.js
@@ -13,10 +13,32 @@ $( function () {
       preparedHtml += '<tr>';
       preparedHtml += '<td class="prepared-uid">' + urlParts[ 0 ] + '</td>';
       preparedHtml += '<td class="prepared-url">' + urlParts[ 1 ] + '</td>';
-      preparedHtml += '<td class="prepared-urn">https://nrs.harvard.edu/' + urlParts[ 0 ] + '</td>';
+      preparedHtml += '<td class="prepared-urn">http://nrs.harvard.edu/urn-3:hul.eresource:' + urlParts[ 0 ] + '</td>';
+      preparedHtml += '<td class="prepared-status">Unsubmitted</td>';
       preparedHtml += '</tr>';
     } );
 
     $( '#prepared tbody' ).append( preparedHtml );
+  } );
+
+  $( '#go' ).click( function( ) {
+    $( '#prepared tbody tr' ).each( function( i ) {
+      var row = $( this );
+
+      $( '#unique_ID' ).val( row.find( '.prepared-uid' ).text() );
+      $( '#URL' ).val( row.find( '.prepared-url' ).text() );
+
+
+      var requestUrl = $( 'form' ).serialize();
+      row.find( '.prepared-status' ).html( 
+        '<iframe width="96" height="48" src="' +
+        'http://wally.hul.harvard.edu:8000/cgi-bin/ernie-prod.pl?' +
+        requestUrl +
+        '"></iframe>'
+      );
+
+    } );
+
+    return false;
   } );
 } );
